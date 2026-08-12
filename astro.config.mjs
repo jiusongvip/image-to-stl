3673f3e832b2cada2ca2846d1247d7be347560dc
DESKTOP-68PFDJP\jiusongPC11 <15795498+yangze9264@user.noreply.gitee.com>
2026-08-12 16:01:50 +0800

    fix: use npx astro build and fix sitemap import order

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   integrations: [tailwind(), react(), sitemap()],
@@ -8,4 +9,3 @@ export default defineConfig({
   site: "https://image-to-stl.com",
   trailingSlash: "never",
 });
-import sitemap from "@astrojs/sitemap";
